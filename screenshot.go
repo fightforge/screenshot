ccad841eb1c020d7639923c6d0044ed824a379d1
fightforge <fightforge@gmail.com>
2022-06-29 11:34:42 +0200

    add x2 func

--- screenshot.go
+++ screenshot.go
@@ -16,3 +16,13 @@ func CaptureDisplay(displayIndex int) (*image.RGBA, error) {
 func CaptureRect(rect image.Rectangle) (*image.RGBA, error) {
 	return Capture(rect.Min.X, rect.Min.Y, rect.Dx(), rect.Dy())
 }
+// CaptureDisplay captures whole region of displayIndex'th display.
+func CaptureDisplayX2(displayIndex int) (*image.RGBA, error) {
+	rect := GetDisplayBounds(displayIndex)
+	return CaptureRectX2(rect)
+}
+
+// CaptureRect captures specified region of desktop.
+func CaptureRectX2(rect image.Rectangle) (*image.RGBA, error) {
+	return Capture(rect.Min.X, rect.Min.Y, rect.Dx()*2, rect.Dy()*2)
+}
