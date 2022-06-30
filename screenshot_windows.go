90b1b7b0b50f663aa8e1101eb2b20018d0622a8f
fightforge <fightforge@gmail.com>
2022-06-30 22:42:24 +0200

    removed comment

--- screenshot_windows.go
+++ screenshot_windows.go
@@ -20,9 +20,6 @@ var (
 )
 
 func Capture(x, y, width, height int) (*image.RGBA, error) {
-        //fmt.Print(width)
-        //fmt.Print(", ")
-        //fmt.Println(height)
 	rect := image.Rect(0, 0, width, height)
 	img, err := util.CreateImage(rect)
 	if err != nil {
